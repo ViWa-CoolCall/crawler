940258eeab603f42f55ed04ec6876f5c58f5966e
Daniel Marques <daniel.brz2009@gmail.com>
2021-08-07 22:34:47 -0300

    stuff

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import puppeteer from 'puppeteer';
 
   await page.goto('https://guarulhos.giap.com.br/apex/pmg/f?p=839:5:::NO:::');
 
-  await page.select('#P5_TIPO_DESPESA', 'R');
+  await page.select('#P5_TIPO_DESPESA', 'O');
 
   await page.click('#P5_COLUNAS_0', { clickCount: 1 });
 
@@ -19,7 +19,10 @@ import puppeteer from 'puppeteer';
 
   await page.select('#P5_MES_FINAL', '3');
 
-  await page.click('#B59474602609746244', { clickCount: 1 });
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('#B59474602609746244'),
+  ]);
 
   await browser.close();
 })();

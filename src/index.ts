44d394e334fb9f077cc79e19e54ee6fda270dbe9
Daniel Marques <daniel.brz2009@gmail.com>
2021-08-29 22:47:20 -0300

    Download working

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,39 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
+  const date = new Date();
+
   const browser = await puppeteer.launch();
 
   const page = await browser.newPage();
 
   await page.goto('https://guarulhos.giap.com.br/apex/pmg/f?p=839:5:::NO:::');
 
+  // await page.screenshot({ path: `images/${date.toLocaleTimeString()}.png` });
+
+  // Selecione o tipo de despesa
   await page.select('#P5_TIPO_DESPESA', 'O');
 
+  // Colunas [ checkboxes ]
   await page.click('#P5_COLUNAS_0', { clickCount: 1 });
 
-  await page.select('#P5_TIPO_RELATORIO', '1');
+  // Selecione a Quebra*
+  await page.select('#P5_TIPO_RELATORIO', '2');
 
+  // Selecione a Sub-Quebra*
   await page.select('#P5_SUB_QUEBRA', '1');
 
+  // Selecionar mês inicial
   await page.select('#P5_MES_INICIAL', '1');
 
+  // Selecionar mês final
   await page.select('#P5_MES_FINAL', '3');
 
-  await Promise.all([
-    page.waitForNavigation(),
-    page.click('#B59474602609746244'),
-  ]);
+  // Tentando navegar p/ outra página
+  await page.screenshot({ path: `images/${date.toLocaleTimeString()}.png` });
+  Promise.all([page.waitForNavigation(), page.click('#B59474602609746244')]);
+
+  await page.click('.download_pdf');
 
   await browser.close();
 })();
---
+++ src/xerebebeu.js
@@ -0,0 +1,42 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+
+async function downloadFile() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto(
+    'https://guarulhos.giap.com.br/apex/pmg/f?p=839:3:4824956594276:',
+    { waitUntil: 'networkidle2' }
+  );
+
+  // Selecionar mês inicial
+  await page.select('#P3_MES_INICIAL', '1');
+  // Selecionar mês final
+  await page.select('#P3_MES_FINAL', '2');
+  // Selecione a Quebra*
+  await page.select('#P3_QUEBRA', '2');
+  // Tipo Receita
+  await page.select('#P3_TIPO_RECEITA', 'O');
+  // Botão de Download
+  await page.click('#B133250412938450781');
+
+  await page.waitForNavigation({ waitUntil: 'load', timeout: 0 });
+
+  await page._client.send('Page.setDownloadBehavior', {
+    behavior: 'allow',
+    downloadPath: path.resolve('./downloads'),
+  });
+
+  await page.click('.download_pdf');
+
+  // Qnd chamado o "close()", o download não está sendo feito
+  // browser.close();
+
+  // ver vídeo gravado sobre a explicação
+  // await page.click('.download_csv');
+  // await page.click('.download_etc');
+  // await page.click('.download_etc');
+}
+
+downloadFile();
